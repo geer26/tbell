e5ad1c6b5e688517568f34fc0ae956f5a2d10aeb
geer26 <gergo.kurucz@gmail.com>
2021-05-26 10:20:30 +0200

    v0_1

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -1,3 +1,17 @@
 console.log("Hello World!");
 
 socket = io();
+
+
+switch_pane('users');
+
+
+function switch_pane(pane){
+
+    $(".pane").each(function() {
+        $(this).hide();
+    });
+
+    $('#'+pane).show();
+
+}
