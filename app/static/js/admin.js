3f4664d73fd4113af3fd3b59752f7b319aa56b13
geer26 <gergo.kurucz@gmail.com>
2021-05-26 10:40:07 +0200

    v0_1

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -8,10 +8,13 @@ switch_pane('users');
 
 function switch_pane(pane){
 
-    $(".pane").each(function() {
-        $(this).hide();
+    $(".pane").each(function(item) {
+        $(item).hide();
+        console.log(item);
     });
 
     $('#'+pane).show();
+
+    console.log('#'+pane);
 
 }
