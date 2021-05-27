c082b92106267c223978fc05b0338dfe5bb2c4b3
geer26 <gergo.kurucz@gmail.com>
2021-05-27 07:36:06 +0200

    v0_1

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -3,15 +3,14 @@ socket = io();
 
 switch_pane('users');
 
+var activepane;
+
 
 function switch_pane(pane){
-
     $("div.pane").hide();
-
     $("i.fas").removeClass('menuicon-active-pane');
-
-    console.log(pane);
     $('#'+pane).show();
     $('#'+pane+'_icon').addClass('menuicon-active-pane');
+    activepane = pane;
 
 }
