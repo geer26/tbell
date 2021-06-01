a848840ce3c164d3ed4250e22e67646d070e0cd9
geer26 <gergo.kurucz@gmail.com>
2021-06-01 11:28:09 +0200

    v0_1

--- app/static/js/userbase.js
+++ app/static/js/userbase.js
@@ -1,7 +1,9 @@
 preload_end();
+switch_pane('workouts_pane')
 
 
 function fetch_hello(){
+    hide_sidebar();
     preload_start();
     fetch('/api')
     .then(response => response.json())
@@ -13,7 +15,22 @@ function fetch_hello(){
 
 
 function hide_sidebar(){
-    $('.user-sidebar').css('left', '-30vw');
-    setTimeout(() => { $('.user-sidebar').css('left',''); }, 400);
+    var x = window.matchMedia("(max-width: 600px)").matches;
+    if (x){
+        $('.user-sidebar').css('left', '-80vw');
+        setTimeout(() => { $('.user-sidebar').css('left',''); }, 600);
+    } else{
+        $('.user-sidebar').css('left', '-30vw');
+        setTimeout(() => { $('.user-sidebar').css('left',''); }, 400);
+    }
     return;
 }
+
+
+function switch_pane(pane){
+    $("div.cont").hide();
+    $('#'+pane).show();
+    activepane = pane;
+    hide_sidebar();
+}
+
