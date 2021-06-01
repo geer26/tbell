7d1d501661cbdbb422ed9a6b3943b3e081afb25f
geer26 <gergo.kurucz@gmail.com>
2021-06-01 09:51:43 +0200

    v0_1

--- app/static/js/userbase.js
+++ app/static/js/userbase.js
@@ -12,35 +12,8 @@ function fetch_hello(){
 }
 
 
-var app = new Vue({
-
-  //element selector (by id in this case) where the app is mounted
-  el: '#app',
-
-  //customize delimiters to avoid conflict with jinja
-  delimiters: ['[[', ']]'],
-
-  //data elements
-  data: {
-    message: 'Hello Vue!'
-  },
-
-  components: {
-    'test-component': httpVueLoader('template.vue')
-  },
-
-  //registered methods and functions
-  methods: {
-
-  }
-
-});
-
-
-Vue.component('todo-item', {
-  // The todo-item component now accepts a
-  // "prop", which is like a custom attribute.
-  // This prop is called todo.
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-})
+function hide_sidebar(){
+    $('.user-sidebar').css('left', '-30vw');
+    setTimeout(() => { $('.user-sidebar').css('left',''); }, 400);
+    return;
+}
